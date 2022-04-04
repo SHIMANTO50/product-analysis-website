c7a1653c1784216b802f6086b600a62bba29958c
SHIMANTO50 <shimantotsp@gmail.com>
2022-04-04 21:09:09 +0600

    Review Updated

--- src/components/AllReviewCard/AllReviewCard.js
+++ src/components/AllReviewCard/AllReviewCard.js
@@ -4,10 +4,10 @@ const AllReviewCard = ({ reviewbyone }) => {
     const { name, review, rating } = reviewbyone;
     return (
         <div>
-            <div className='w-2/4 rounded-2xl shadow-lg p-4 bg-green-300'>
-                <p className='font-bold'>Name:{name}</p>
-                <p className='font-bold'>Review:{review}</p>
-                <p className='font-bold'>Ratings:{rating}</p>
+            <div className='w-2/4 rounded-2xl shadow-lg p-4 bg-green-300 h-72 mb-7'>
+                <p><span className='font-bold'>Name:</span> {name}</p>
+                <p><span className='font-bold'>Review-Details:</span> {review}</p>
+                <p><span className='font-bold'>Ratings:</span> {rating}</p>
             </div>
         </div>
     );
--- src/components/ReviewCard/ReviewCard.js
+++ src/components/ReviewCard/ReviewCard.js
@@ -6,9 +6,9 @@ const ReviewCard = ({ reviewbyone }) => {
     return (
         <>
             <div className='w-2/4 rounded-2xl shadow-lg p-4 bg-green-300'>
-                <p className='font-bold'>Name:{name}</p>
-                <p className='font-bold'>Review:{review}</p>
-                <p className='font-bold'>Ratings:{rating}</p>
+                <p><span className='font-bold'>Name:</span> {name}</p>
+                <p><span className='font-bold'>Review-Details:</span> {review}</p>
+                <p><span className='font-bold'>Ratings:</span> {rating}</p>
             </div>
 
         </>

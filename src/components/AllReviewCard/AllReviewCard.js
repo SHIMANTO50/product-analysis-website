6dc8a8de7278bd1813e22794b9285146ebb25e7a
SHIMANTO50 <shimantotsp@gmail.com>
2022-04-04 21:49:19 +0600

    Review image added

--- src/components/AllReviewCard/AllReviewCard.js
+++ src/components/AllReviewCard/AllReviewCard.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 const AllReviewCard = ({ reviewbyone }) => {
-    const { name, review, rating } = reviewbyone;
+    const { name, review, rating, img } = reviewbyone;
     return (
         <div>
-            <div className='w-2/4 rounded-2xl shadow-lg p-4 bg-green-300 h-72 mb-7'>
+            <div className='w-2/4 rounded-2xl shadow-lg p-4 bg-green-300 h-[500px] mb-7'>
+                <img src={img} alt="" />
                 <p><span className='font-bold'>Name:</span> {name}</p>
                 <p><span className='font-bold'>Review-Details:</span> {review}</p>
                 <p><span className='font-bold'>Ratings:</span> {rating}</p>
--- src/components/ReviewCard/ReviewCard.js
+++ src/components/ReviewCard/ReviewCard.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 
 const ReviewCard = ({ reviewbyone }) => {
-    const { name, review, rating } = reviewbyone;
+    const { name, review, rating, img } = reviewbyone;
     return (
         <>
             <div className='w-2/4 rounded-2xl shadow-lg p-4 bg-green-300'>
+                <img src={img} alt="" />
                 <p><span className='font-bold'>Name:</span> {name}</p>
                 <p><span className='font-bold'>Review-Details:</span> {review}</p>
                 <p><span className='font-bold'>Ratings:</span> {rating}</p>
